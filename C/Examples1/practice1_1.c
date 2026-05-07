4b8021600e87137d670473288104c1a57b88eaec
t25b364a <t25b364a@mail.cc.niigata-u.ac.jp>
2026-05-07 11:21:48 +0900

    for assignment

--- C/Examples1/practice1_1.c
+++ C/Examples1/practice1_1.c
@@ -1,5 +1,5 @@
 ﻿/*
- * example1_1 copy.c
+ * practice1_1 .c
  *
  * プログラミングBI 演習課題1-1
  *
@@ -14,12 +14,15 @@ int main()
 	double x; /* 独立変数 */
 	double y; /* 従属変数 (関数の近似値) */
 	int n; /* 近似次数 */
+	double z; /*xの二乗の変数*/
 
 	/* 途中の計算に必要な変数の宣言 */
 	int i; /* 繰り返し変数の宣言 */
+	int j; /*繰り返し変数を二倍したもの*/
 	double f; /* 階乗値 */
 	double p; /* x の累乗値 */
-
+	double q; /*各項の値*/
+	double r; /*偶数番目の項は負、奇数番目は正にした値/
 	/* データの読み込み(倍精度, 整数) */
 	scanf("%lf%d",&x,&n);
 
@@ -27,10 +30,17 @@ int main()
 	y = 1.0;
 	f = 1.0;
 	p = 1.0;
+	z = x*x; 
 	for (i=1; i<=n; i++) {
 		f = f * (double)i; /* f *= (double)i; とも書ける */
-		p = p * x; /* p *= x; とも書ける */
-		y = y + p / f; /* y += p / f; とも書ける */
+		j = i*2;
+		f = f * (double)j;
+		p = p * z; /* p *= z; とも書ける */
+		q = p/f;
+		for (j=2; j<=i; j++) {
+			r = q*-1;
+		}
+		y = y + r; /* y += r; とも書ける */
 	}
 
 	/* e^x の近似値の表示 */
