6284cb7599abca348bba9465455689afab08d061
t25b364a <t25b364a@mail.cc.niigata-u.ac.jp>
2026-05-07 11:22:02 +0900

    for assignment

--- C/Examples1/practice1_1.c
+++ C/Examples1/practice1_1.c
@@ -13,16 +13,15 @@ int main()
 	/* 変数宣言 */
 	double x; /* 独立変数 */
 	double y; /* 従属変数 (関数の近似値) */
+	double z; /* xの二乗の変数*/
 	int n; /* 近似次数 */
-	double z; /*xの二乗の変数*/
 
 	/* 途中の計算に必要な変数の宣言 */
 	int i; /* 繰り返し変数の宣言 */
-	int j; /*繰り返し変数を二倍したもの*/
 	double f; /* 階乗値 */
 	double p; /* x の累乗値 */
-	double q; /*各項の値*/
-	double r; /*偶数番目の項は負、奇数番目は正にした値/
+	double q; /* 各項の値 */
+	double r; /* 各項の符号を表す*/
 	/* データの読み込み(倍精度, 整数) */
 	scanf("%lf%d",&x,&n);
 
@@ -30,21 +29,19 @@ int main()
 	y = 1.0;
 	f = 1.0;
 	p = 1.0;
+	r = -1.0;
 	z = x*x; 
 	for (i=1; i<=n; i++) {
-		f = f * (double)i; /* f *= (double)i; とも書ける */
-		j = i*2;
-		f = f * (double)j;
+		f = f * (2 * (double)i - 1); /* f *= 2 * (double)i - 1; とも書ける */
+		f = f * (2 * (double)i); /* f *= 2 * (double)i; とも書ける */
 		p = p * z; /* p *= z; とも書ける */
-		q = p/f;
-		for (j=2; j<=i; j++) {
-			r = q*-1;
-		}
-		y = y + r; /* y += r; とも書ける */
+		q = p/f; /* 各項を求めた */
+		y = y + r * q; /* 各項の値を加えていく */
+		r = -r; /* 各項の符号の調整 */
 	}
 
 	/* e^x の近似値の表示 */
-	printf("マクローリン展開による次数 %d までの e^(%g) の近似値\n"
+	printf("マクローリン展開による次数 %d までの cos(%g) の近似値\n"
 		"    =%21.16g\n", n, x, y);
 	
 	return 0;
