24e3622841e9a8e0946aa8e46254ae3d4fa26a14
t25b364a <t25b364a@mail.cc.niigata-u.ac.jp>
2026-05-10 18:44:21 +0900

    for assignment

--- C/Examples1/practice1_3.c
+++ C/Examples1/practice1_3.c
@@ -9,80 +9,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DELTA 1E-15 /* 誤差の上限 */
-#define absdif(x,y) ((x>y) ? (x-y) : (y-x)) /* 差分絶対値 */
-
-/* 平方根を求める関数 */
-double mysqrt(double y) {
-
-	int k = 0; /* 反復回数 */
-	double x = 1.0; /* 平方根 */
-	double e = absdif(x,y/x); /* 誤差 */
-
-	/* 反復計算 */
-	printf(" %21.16g（反復回数 %d 回）\n", x, k);
-	while (e >= DELTA && k < 1000) {
-		k++;
-		x = ( x + y/x )/2.0; /* 値の更新 */
-		e = absdif( x, y/x ); /* 誤差 */
-		printf(" %21.16g（反復回数 %d 回）\n", x, k);
-	}
-	x = ( x + y/x )/2.0; /* 値の更新 */
-
-	return x;
+int hex_to_int(char c)
+{	/* ASCIIを利用 */
+    if ('0' <= c && c <= '9') return c - '0';
+    if ('A' <= c && c <= 'F') return c - 'A' + 10;
+    if ('a' <= c && c <= 'f') return c - 'a' + 10;
+    return -1; /* 異常あり*/
 }
 
-/* メイン関数 */
 int main()
 {
-	/* 変数宣言 */
-	double a,b,c; /* 入力値 */
-	double d; /*判別式Dの値*/
-	double p,q,r; /*二次方程式の解を表す*/
-	double x; /* 平方根 */
+    int n_int, n_frac; /* 整数部,小数部の桁数 */
+    char *s_int, *s_frac; /* 整数部を文字列として受け取った */
+    int *a_int, *a_frac; /* 小数部を文字列として受け取った */
+    int i; /* 変数*/
+    double value = 0.0; /* 10進数に直すための初期値*/
+    double p,q; /* それぞれの桁について10進数に変えるための値*/
 
-	/* 平方根を求める値の読み込み */
-	printf("実数値aを入力して下さい: ");
-	scanf("%lf",&a);
-	printf("実数値bを入力して下さい: ");
-	scanf("%lf",&b);
-	printf("実数値cを入力して下さい: ");
-	scanf("%lf",&c);
+    printf("整数部の桁数：");
+    scanf("%d", &n_int);
 
-	/* 入力データの確認 */
-	if ( a == 0 ) {
-		printf("これは二次方程式ではありません。終了します。\n");
-		exit(EXIT_SUCCESS);
-	}
+    printf("小数部の桁数：");
+    scanf("%d", &n_frac);
 
-	/* 判別式Dの計算 */
-	d = b * b - 4 * a * c;
+	/*動的領域確保*/
+	s_int = (char*)malloc(sizeof(char) * n_int);
+	s_frac = (char*)malloc(sizeof(char) * n_frac);
+	a_int = (int*)malloc(sizeof(int) * n_int);
+	a_frac = (int*)malloc(sizeof(int) * n_frac);
 
-	/*解の実部を求める*/
-	p = - b / (2 * a);
+    /* 配列への代入*/
+	printf("整数部を1文字ずつ入力（0〜9, A〜F）：\n");
+    for (i = 0; i < n_int; i++) {
+        scanf(" %c", &s_int[i]);
+        a_int[i] = hex_to_int(s_int[i]);
+    }
 
-	/*判別式Dの評価*/
-	if (d > 0) {
-		printf("二次方程式の解：\n");
-		x = mysqrt(d);
-		q = x / (2 * a);
-		r = p + q;
-		printf("x1 = %21.16g \n", r);
-		r = - r;
-		printf("x2 = %21.16g \n", r);
-	}
-	else if (d == 0) {
-		printf("二次方程式の解：\n");
-		printf("X = %21.16g(重解) \n", p);
-	}
-	else {
-		printf("二次方程式の解：\n");
-		d = -d;
-		x = mysqrt(d);
-		q = x / (2 * a);
-		printf("x1 = %21.16g+%21.26gj \n", p,q);
-		printf("x2 = %21.16g-%21.26gj \n", p,q);
-	}
-	
-	return 0;
+    printf("小数部を1文字ずつ入力（0〜9, A〜F）：\n");
+    for (i = 0; i < n_frac; i++) {
+        scanf(" %c", &s_frac[i]);
+        a_frac[i] = hex_to_int(s_frac[i]);
+    }
+
+    /* 整数部 */
+    p = 1.0;
+    for (i = n_int - 1; i >= 0; i--) {
+        value += a_int[i] * p;
+        p *= 16.0;
+    }
+
+    /* 小数部 */
+    q = 1.0 / 16.0;
+    for (i = 0; i < n_frac; i++) {
+        value += a_frac[i] * q;
+        q /= 16.0;
+    }
+
+    printf("10進数 = %f\n", value);
+
+	/* 解放を行う*/
+    free(s_int);
+    free(s_frac);
+    free(a_int);
+    free(a_frac);
+
+    return 0;
 }
