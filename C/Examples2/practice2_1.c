b615d67bfe0a33f0e572f3b7d7f3d957a2b91716
t25b364a <t25b364a@mail.cc.niigata-u.ac.jp>
2026-05-22 11:45:37 +0900

    for assignment

--- C/Examples2/practice2_1.c
+++ C/Examples2/practice2_1.c
@@ -19,73 +19,104 @@ double getNorm2(double*pVector);
 int main()
 {
   /* 変数宣言 */
-  int i,j; /* ループ変数 */ 
+  int i, j; /* ループ変数 */ 
   int k; /* 繰り返し回数 */
-  double *pVector; /* ベクトルのポインタ */
+  double *pVector_b, *pVector_p, *pVector_q, *pVector_x, *pVector_y, *pVector_z; /* ベクトルのポインタ */
+  double A[2][2] = {{1.0, 2.0}, {3.0, 4.0}}; /* 行列A */
   char mode;
 
   /* ベクトルのための動的領域確保 */
-  pVector = (double*)malloc(sizeof(double)*2);
-
-
+  pVector_b = (double*)malloc(sizeof(double)*2);/* bの配列*/
+  pVector_p = (double*)malloc(sizeof(double)*2);/* pの配列*/
+  pVector_x = (double*)malloc(sizeof(double)*2);/* xの配列*/
+  pVector_y = (double*)malloc(sizeof(double)*2);/* yの配列*/
+  pVector_z = (double*)malloc(sizeof(double)*2);/* zの配列*/
+  
   do {
     /* ベクトルbの要素の読み込み */
     printf("ベクトルb の要素の読み込み\n");
-    inputVec(pVector);
-
+    inputVec(pVector_b);
+    
     /* 繰り返し回数 */
     printf("繰り返し回数を入力してください. (>0): ");
     scanf("%d", &k);
+   } while( k < 1 );
     
-    /* ベクトルの長さ（ノルム）の計算と表示 */
+   /* ベクトルxの表示 */
     for (i = 0; i < k; i++) {
       printf("繰り返し回数: %d\n", i + 1);
       printf("ベクトルx:");
       for (j = 0; j < 2; j++) {
-        printf("%d番目の要素: %f\n", j + 1, pVector[j]);
+        printf("%d番目の要素: %f\n", j + 1, pVector_x[j]);
       }
-      printf("距離: %f\n\n", getNorm2(pVector));
+      getMtxVecProd2(pVector_y, A, pVector_x); /* 行列Aとベクトルxの積を計算 */
+      getVecDiff2(pVector_z, pVector_y, pVector_b); /*ベクトルbとベクトルyの差分を計算 */
+      getMtxVecProd2(pVector_p, A, pVector_z); /*ベクトルzと行列Aの積を計算 */
+      /* ベクトルqの計算 */
+      for (i = 0; i < 2; i++){
+        pVector_q[i] = pVector_p[i] / 30;
+      }
+      /* ベクトルxの更新 */
+      for (i = 0; i < 2; i++){
+        pVector_x[i] = pVector_x[i] - pVector_q[i];
+      }
+
+      printf("距離: %f\n\n", getNorm2(pVector_z));
     }
 
-    /* 継続の確認 */
-    printf("継続しますか？['y'で継続]: ");
-    scanf(" %c", &mode); /* %cの前にスペースを入れる */
-    printf("\n");
-  } while ( mode=='y' || mode=='Y' );
-
   /* 領域解放 */
-  free(pVector);
-    
+  free(pVector_b);
+  free(pVector_p);
+  free(pVector_q);
+  free(pVector_x);
+  free(pVector_y);
+  free(pVector_z);
+
   return 0;
 }
 
 /* ベクトルの要素を問い合わせる関数 */
-void inputVec(double* pVector)
+void inputVec(double*pVector)
 {
-    int iDim;
-    for(iDim=0; iDim<2; iDim++) {
-      printf("%d番目の要素を入力して下さい: ", iDim+1);
-      scanf("%lf", &pVector[iDim] );
+  int i;
+  for(i = 0; i < 2; i++) {
+    printf("%d番目の要素を入力して下さい: ", i + 1);
+    scanf("%lf", &pVector[i] );
+  }
+  printf("\n");
+}
+
+/* 行列とベクトルの積を計算する関数 */
+void getMtxVecProd2(double*pVectorOut,double pMatrix[][2],double*pVectorIn)
+{
+  int i, j;
+  for(i = 0; i < 2; i++) {
+    pVectorOut[i] = 0.0;
+    for(j = 0; j < 2; j++) {
+      pVectorOut[i] += pMatrix[i][j] * pVectorIn[j];
     }
-    printf("\n");
+  }
+}
+
+/* 二つのベクトルの差分を計算する関数 */
+void getVecDiff2(double*pVectorOut,double*pVectorIn1,double*pVectorIn2)
+{
+  int i;
+  for(i = 0; i < 2; i++) {
+    pVectorOut[i] = pVectorIn1[i] - pVectorIn2[i];
+  }
 }
 
 /* ベクトルの長さ（ノルム）を計算する関数 */
 double getNorm2(double*pVector)
 {
+int i;
+double sqrdSum;
 
-  static int nCalls = 0; /* 呼び出された回数（静的変数）*/
-  int iDim,k;
-  double sqrdSum;
-
-  /* 繰り返し回数の更新と表示 */
-  nCalls++;
-  printf("繰り返し回数: %d\n", nCalls);
-
-  /* ベクトルの内積の計算 */
-  sqrdSum=0.0;
-  for(iDim=0; iDim<2; iDim++)
-    sqrdSum += pVector[iDim] * pVector[iDim];
+/* ベクトルの内積の計算 */
+sqrdSum = 0.0;
+for(i = 0; i < 2; i++)
+  sqrdSum += pVector[i] * pVector[i];
 
   return sqrt(sqrdSum);
 }
