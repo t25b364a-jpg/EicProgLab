f5b665dcd49c44e9a1728cebcfef7b143602043e
t25b364a <t25b364a@mail.cc.niigata-u.ac.jp>
2026-06-18 11:07:07 +0900

    for assignmnet

--- Python/Examples1/practice1_2.py
+++ Python/Examples1/practice1_2.py
@@ -23,24 +23,24 @@ def main(args):
     ω = MyComplex(float(args[4]), 0)
 
     # 各複素数の内容を表示して確認
-    print('電源電圧 |E| \t= ' + str(SRC_VOLTAGE) +' V')
-    print('抵抗 R \t= ' + str(R.real) + ' Ω')
-    print('インダクタ L \t= ' + str(L.real) + ' H')
-    print('コンデンサ C \t= ' + str(C.real) + ' F')
-     print('角周波数 ω \t= ' + str(ω.real) + ' rad/s')
+    print('電源電圧 E = ' + str(SRC_VOLTAGE) +' V')
+    print('抵抗 R = ' + str(float(args[1])) + ' Ω')
+    print('インダクタ L = ' + str(float(args[2])) + ' H')
+    print('コンデンサ C = ' + str(float(args[3])) + ' F')
+    print('角周波数 ω = ' + str(float(args[4])) + ' rad/s')
 
     # 抵抗にかかる電圧の複素数を求める
     X = (ω * L) - (MyComplex(1.0, 0.0) / (ω * C))
-    V = (R * SRC_VOLTAGE) / (R + MyComplex(0, X.real))
-    print('R にかかる電圧 V \t= ' + str(V) + ' V')
+    V = (R * SRC_VOLTAGE) / (R + MyComplex(0, X.abs()))
+    print('R にかかる電圧 V = ' + str(V) + ' V')
 
     #電圧の大きさ[V]を求める
     absV = V.abs()
-    print(' 大きさ : \t= ' + str(absV) + ' V')
+    print(' 大きさ : ' + str(absV) + ' V')
 
     #電圧のの偏角[rad]を求める
     phaseV = V.ang()
-    print(' 偏角 : \t= ' + str(phaseV) + ' rad')
+    print(' 偏角   : ' + str(phaseV) + ' rad')
 
 if __name__ == '__main__':
     main(sys.argv)
