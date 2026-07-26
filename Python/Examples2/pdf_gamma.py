1fa6276e1c4e5af82a7aca167d26d0504ea48f84
t25b364a <t25b364a@mail.cc.niigata-u.ac.jp>
2026-07-26 17:58:02 +0900

    for assignment

--- Python/Examples2/pdf_gamma.py
+++ Python/Examples2/pdf_gamma.py
@@ -1,20 +1,21 @@
 from ipdf import IProbabilityDensityFunction
 import random
 
-class Beta(IProbabilityDensityFunction):
-    
+class Gamma(IProbabilityDensityFunction):
+
     def parameter_inquiry(self):
-        print('ベータ分布のパラメータ(α,β)を入力してください')
-        self.alpha = float(input('α: '))
-        self.beta  = float(input('β: '))
+        print('ガンマ分布のパラメータ(k, θ)を入力してください')
+        self.k = float(input('k: '))
+        self.sita = float(input('θ: '))
 
     def draw_sample(self):
-        return random.betavariate(self.alpha,self.beta)
-                    
+        return random.gammavariate(self.k, self.sita)
+
     @property
     def name(self):
-        return 'ベータ分布'  
-        
-    @property    
+        return 'ガンマ分布'
+
+    @property
     def analytical_expectation(self):
-        return self.alpha/(self.alpha+self.beta)
+        return self.k * self.sita
+
